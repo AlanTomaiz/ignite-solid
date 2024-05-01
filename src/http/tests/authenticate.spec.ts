3fce0b9e798ee1335ac472d1358fad39686ba4b0
Alanderson Tomaiz <alan_tomaiz@hotmail.com>
2024-05-01 10:48:59 -0300

    feat: create first tests e2e

--- src/http/tests/authenticate.spec.ts
+++ src/http/tests/authenticate.spec.ts
@@ -1,3 +1,29 @@
-import { test } from 'vitest'
+import { app } from '@/app'
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('Authenticate User', {})
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate user', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'johndoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+    expect(response.text).toEqual(expect.stringContaining('eyJh'))
+  })
+})
